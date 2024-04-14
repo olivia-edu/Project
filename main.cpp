208da4a59ea9dba74e280177114c836541f74660
Olivia <omkramer@alaska.edu>
2024-04-14 03:45:26 -0800

    feature:Combat. Implemented levels mechanic, player scaling, enemy damage, and enemy health. - Tony 4/13 @ 4:00 PM. bug:Opening. Added comments, unified own code format with Tony's. - Olivia 4/13 @ 10:30 PM

--- main.cpp
+++ main.cpp
@@ -2,25 +2,26 @@
 #include <ctime>
 #include <string>
 
-
-void printCharacters(int num, char character) //to assist gameTitle() format
+// Used to format ASCII art
+void printCharacters(int num, char character) 
 {
   for (int i = 0; i < num; i++) {
     cout << character;
   }
 }
 
-void gameTitle(const string& title, const string& subtitle) //ASCII game title
+// Used to print ASCII art game title
+void gameTitle(const string& title, const string& subtitle) 
 {
-  //Variables 
-  int title_length = title.length();
-  int subtitle_length = subtitle.length();
-  int line_width = 77;
-  int sub_line_width = subtitle_length +2;
-  int title_left_padding = (line_width - title_length) / 2;
-  int title_right_padding = line_width - title_left_padding - title_length;
-  int subtitle_left_padding = (sub_line_width - subtitle_length) / 2;
-  int subtitle_right_padding = sub_line_width - subtitle_left_padding - subtitle_length;
+  // Game title variables 
+  int title_length = title.length();                                                              // gets title length
+  int subtitle_length = subtitle.length();                                                        // gets subtitle length
+  int line_width = 77;                                                                            // sets width of title line
+  int sub_line_width = subtitle_length +2;                                                        // sets width of subtitle
+  int title_left_padding = (line_width - title_length) / 2;                                       // maintains ASCII art by padding to the left of the title
+  int title_right_padding = line_width - title_left_padding - title_length;                       // maintains ASCII art by padding to the right of the title
+  int subtitle_left_padding = (sub_line_width - subtitle_length) / 2;                             // maintains ASCII art by padding to the left of the subtitle
+  int subtitle_right_padding = sub_line_width - subtitle_left_padding - subtitle_length;          // maintains ASCII art by padding the the right of the subtitle
   
   cout << "          0";
   printCharacters(title_left_padding, ' ');
@@ -43,6 +44,7 @@ void gameTitle(const string& title, const string& subtitle) //ASCII game title
   cout << "0" << endl;
 }
 
+// Used to introduce the game and get the PC's name
 void Intro(const string& title, const string& subtitle, string& name)
 {
   cout << "Brief Opening Statement, used to establish a sense of the setting. \n(Press Enter to Continue.)\n";
@@ -53,16 +55,19 @@ void Intro(const string& title, const string& subtitle, string& name)
   cout << "\nWelcome to (" << title<< "). Are you ready to play? \n(Press Enter to Continue.)"; 
   while (cin.get() != '\n') {}
   
-  cout << "\nPlease type the full name of your character. After typing your name, press enter to continue. \n\n"; // Later ask for weapon type/other attribute
+  cout << "\nPlease type the full name of your character. After typing your name, press enter to continue. \n\n"; 
   getline(cin, name);
   
   cout << "\n" << name << ", Your adventure begins! \n(Press Enter to Continue.)" << endl;
   while (cin.get() != '\n') {}
 }
 
+// Used to print ASCII art banner
 void printBanner(const string& sentence) 
 {
-  int padding = (65 - sentence.length()) / 2;
+  // Art banner variables
+  int padding = (65 - sentence.length()) / 2;                                                    // establishes padding to center sentence
+  
   if (padding < 0) {
     padding = 0;
   }
@@ -92,10 +97,12 @@ void printBanner(const string& sentence)
   cout << " ( ~ )" << endl;
 }
 
+// Used to let PC investigate doors
 void investigateDoor()
 {
-  int door_num;
-  bool valid_input = false;
+  // Door investigation variables
+  int door_num;                                                                           // holds user selected door number
+  bool valid_input = false;                                                               // helps to test for input validity
 
   do {
     cout << "\nSelect a door to investigate. (1/2/3/4): ";
@@ -125,10 +132,12 @@ void investigateDoor()
   } while (!valid_input);
 }
 
+// Used to let PC select door to enter
 void selectDoor()
 {
-  int door_num;
-  bool valid_input = false;
+  // Door selection variables
+  int door_num;                                                                        // holds user selected door number
+  bool valid_input = false;                                                            // helps to test for input validity
 
   do {
     cout << "\nSelect a door to enter. (1/2/3/4): ";
@@ -158,11 +167,13 @@ void selectDoor()
   } while (!valid_input);
 }
 
+// Used to seperate UI elements
 void printDivider(void)
 {
     std::cout << "***************************************************************************\n";
 }
 
+// Used to check if player or enemy is alive
 bool deathCheck(int currentHealth)
 {
     if (currentHealth <= 0)
@@ -206,69 +217,81 @@ int main()
     // ASCII art for display
     printBanner(variable_sentence);
     
-    // Opening - combat divider
+    // Divides opening and combat
     printDivider();
 
     // Battle variables
-    int decisions, l_att, h_att, block, heal, enemy_turn;
+    int decisions, enemy_turn;                             // used in switch statements to determine player and enemy action
+    int l_att, h_att, heal;                                // used to store battle values
     int chance1;                                           // used to store chance to hit for player and enemy
-    int health = 100; 
-    int enemy_health = 70 + rand() % 45;                   // determines the enemies health for the start of the encounter (70 - 114)
-    int health1 = 100;
+    int health = 100;                                      // player health
+    int level = 1;                                         // used to store current player level
 
-    // Start battle
-    cout << "You encountered an enemy" << endl;
-    do {
-        // Prompt user for action
-        cout << "\nEnemy has " << enemy_health << " hp.\t\tPlayer has " << health << " hp.\n\n";
-        cout << "Choose 1 for light attack\tChoose 2 for heavy attack\n\nChoose 3 to heal\t\tChoose any other number to rage quit lol\n\n";
-        printDivider();
-        cout << "Your action: ";
-        cin >> decisions;
-        cout << endl;
-
-        // Determine player action
-        switch (decisions) 
+    // Begin battle
+    bool gameOver = false;                                 // used for outer while loop condition to end the game
+    while (!gameOver)
+    {
+        int levelMod = (level - 1) * 2;                        // used to scale game according to level
+        int enemy_health = 70 + rand() % 45;                   // determines the enemies health for the start of the encounter (70 - 114)
+        enemy_health += levelMod;                              // scale enemy health
+        cout << "LEVEL: " << level << "\t\t\t";
+        cout << "You encountered an enemy" << endl;
+ 
+        // Start Turn
+        do
         {
-            // Perform light attack
-            case 1: 
+            // Prompt user for action
+            cout << "\nEnemy has " << enemy_health << " hp.\t\tPlayer has " << health << " hp.\n\n";
+            cout << "Choose 1 for light attack\tChoose 2 for heavy attack\n\nChoose 3 to heal\t\tChoose any other number to rage quit lol\n\n";
+            printDivider();
+            cout << "Your action: ";
+            cin >> decisions;
+            cout << endl;
+
+            // Determine player action
+            switch (decisions)
+            {
+                // Perform light attack
+            case 1:
             {
                 cout << "You have chosen light attack\n" << endl;
                 int chance1 = 1 + rand() % 10;                    // Get number between 1 and 10 inclusive 
                 if (chance1 <= 9)                                 // 90% chance to hit
                 {
                     int l_att = 7 + rand() % 13;                  // Player damage output 7 - 19
+                    l_att += levelMod;                            // Scale damage
                     enemy_health = enemy_health - l_att;
                     cout << "The attack was a success!  You dealt " << l_att << " damage.\n";
                 }
-                else 
+                else
                 {
                     cout << "Your attack missed.\n"; // 10% chance to miss
                 }
-                break; 
+                break;
             }
             // Perform heavy attack
-            case 2: 
+            case 2:
             {
                 cout << "You have chosen heavy attack.\n";
                 int chance1 = rand() % 4;                   // Get number between 0 and 3
                 if (chance1 <= 1)                           // 50% chance to hit
                 {
                     int h_att = 30 + rand() % 10;           // Player damage output 30 - 39
+                    h_att += levelMod * 2;                  // Scale damage, multiplied by 2 again for heavy scaling
                     enemy_health = enemy_health - h_att;
                     cout << "The attack was a success!  You dealt " << h_att << " damage.\n";
                 }
-                else 
+                else
                 {
                     cout << "Your attack missed.\n";
                 }
-                break; 
+                break;
             }
             // Heal player
-            case 3: 
+            case 3:
             {
                 cout << "You have chosen to heal.\n";
-                if (health != 100) 
+                if (health != 100)
                 {
                     int chance1 = 1 + rand() % 2;  // Get number 1 or 2
                     if (chance1 == 2)              // 50% chance
@@ -278,7 +301,7 @@ int main()
                         cout << "You healed " << heal << " health.\n";
                         cout << "You have " << health << " hp left.\n";
                     }
-                    else 
+                    else
                     {
                         int heal = rand() % 14;  // Heal for 0 to 13
                         health = health + heal;
@@ -287,7 +310,7 @@ int main()
                     }
                 }
                 // Display if player is at full health
-                else 
+                else
                 {
                     cout << "You just wasted your turn, better luck next time.\n";
                 }
@@ -297,84 +320,94 @@ int main()
                     health = 100;               // Update player health so never greater than 100
                     cout << "\n\n\nYou have " << health << " left\n\n\n";
                 }
-                break; 
+                break;
             }
 
             // Display if player enters a number besides 1, 2, or 3
-            default: 
+            default:
             {
-                cout << "Rage quitter lol, hope the game was worth it.";
+                cout << "Rage quitter lol, hope the game was worth it.\n\n";
                 return 0;
             }
-        }
-        // If enemy is dead, end battle
-        if (deathCheck(enemy_health))
-        {
-            break;
-        }
-        else
-        {
-            cout << "Enemy has " << enemy_health << " hp left, it is now the enemy's turn.\n";
-        }
+            }
+            // If enemy is dead, end battle
+            if (deathCheck(enemy_health))
+            {
+                cout << "\nThe enemy has 0 health remaining. They have been defeated.\n\n";
+                printDivider();
+                cout << endl;
+                break;
+            }
+            else
+            {
+                cout << "Enemy has " << enemy_health << " hp left, it is now the enemy's turn.\n";
+            }
 
-        // Start enemy turn
-        int enemy_turn = 1 + rand() % 3;                // Get number 1, 2, or 3. Used to determine enemy action
-        switch (enemy_turn) 
-        {
-            // Enemy light attack
-            case 1: 
+            // Start enemy turn
+            int enemy_turn = 1 + rand() % 3;                // Get number 1, 2, or 3. Used to determine enemy action
+            switch (enemy_turn)
+            {
+                // Enemy light attack
+            case 1:
             {
                 int chance1 = rand() % 2;  // Get number 0 or 1
                 if (chance1 == 1)          // 50% chance to hit
                 {
                     int l_att = 7 + rand() % 13;    // Enemy attack 7 - 19 damage
+                    l_att += levelMod;
                     health = health - l_att;
                     cout << "The enemy attacked! They dealt " << l_att << " damage.\n";
 
                 }
-                else 
+                else
                 {
                     cout << "Their attack missed, it is now your turn.\n";
                 }
-                break; 
+                break;
             }
 
             // Enemy heavy attack
-            case 2: 
+            case 2:
             {
                 int chance1 = rand() % 4;     // Get number between 0 and 3
                 if (chance1 == 0)             // Chance to hit is 25%
                 {
                     int h_att = 35 + rand() % 5;    // Enemy attack 35 - 39 damage
+                    h_att += levelMod * 2;          // Scale damage, times 2 for heavy scaling
                     health = health - h_att;
                     cout << "They actually hit you with a heavy attack and dealt " << h_att << " damage.\n";
                 }
-                else 
+                else
                 {
                     cout << "The dumbass missed a heavy attack, lets show them how its done.\n";
                 }
-                break; 
+                break;
             }
 
             // Chance for enemy to waste turn
-            case 3: 
+            case 3:
             {
                 cout << "The enemy is just standing there... menacingly!\n";
-                break; 
+                break;
             }
-        }
-        // If player is dead, end battle
-        if (deathCheck(health))
-        {
-            break;
-        }
-        else
-        {
-            cout << "You now have " << health << " hp left, it is now your turn.\n";
-        }
-    } 
-    while (health >= 0 && enemy_health >= 0);  // Continue until player or enemy health is negative or 0
+            }
+            // If player is dead, end battle
+            if (deathCheck(health))
+            {
+                gameOver = true;
+                break;
+            }
+            else
+            {
+                cout << "You now have " << health << " hp left, it is now your turn.\n";
+            }
+        } while (health > 0 && enemy_health > 0);  // Continue until player or enemy health is negative or 0
 
+        level++;                // increment level if enemy defeated, maybe add line resetting player health to 100
+    }
     // Placeholder for game over screen
-    cout << "Game over" << endl;
+    cout << endl;
+    printDivider();
+    cout << "\n\nYour health is now 0, you have been defeated.\n\n";
+    cout << "Game over, you completed " << level - 1 << " levels.\n";
 }
